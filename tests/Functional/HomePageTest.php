d008f059e73c2a7894fd55a67ab7b5676fef6828
Antonio6999 <antoine.chabeauti@gmail.com>
2023-10-02 18:35:08 +0200

    functional

---
+++ tests/Functional/HomePageTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomePageTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello World');
+        $this->assertSelectorExists('head > meta[charset="UTF-8"]');
+        $this->assertSelectorExists('footer.footer');
+    }
+}
